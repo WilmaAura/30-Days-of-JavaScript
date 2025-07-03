f7658eec7e803f49e2a4bdc2196210e6807e4a23
Wilma <mornlitoralis@gmail.com>
2025-07-03 09:27:28 +0700

    Initial commit

---
+++ 01_Start/main.js
@@ -0,0 +1,9 @@
+class measurement{
+    constructor (type, value1, value2){
+        
+    }
+}
+function rectangleArea(l, w) {
+  return l * w;
+}
+console.log(rectangleArea(5, 2));
---
+++ 01_Start/variable.js
@@ -0,0 +1,29 @@
+// Declaring variables with number values
+let x, y, z;
+x = 5;
+y = 7;
+z = x + y;
+console.log(z);
+
+// Declaring variables with different data types
+let firstName = "Wilma";
+let lastName = "Auraruna Khalif";
+let age = 20;
+let address = "Purwodadi";
+let favoriteFood = "Milkfish presto";
+
+// Declaring const variables
+const freezingPoint = 0; //in Celcius
+const boilingPoint = 100; //in Celcius
+const meltingPoint = 1371; //in Celcius
+const gravity = 9.8;
+
+// Arithmatic
+let exponentation = 9 ** 2;
+let increment = 5;
+increment++; //
+increment += 10;
+console.log(increment);
+increment **= 2;
+console.log(exponentation);
+console.log(increment);
